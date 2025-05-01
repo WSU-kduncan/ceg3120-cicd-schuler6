afb0eaafbfd492e98e1bb4269971242399c7c4b5
Ubuntu <ubuntu@ip-172-31-17-145.ec2.internal>
2025-05-01 01:27:19 +0000

    fixed deploy.sh (should be no race conditions now)

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,9 +1,20 @@
-#! /bin/bash
+#!/bin/bash
+# deployment/deploy.sh
 
-# kill the old container process - stopped & then removed
-docker stop birdapp || true
-docker rm birdapp || true
-# pull fresh image
-docker pull schuler6/schuler6-3120:latest
-# run new container by name, with restart automatic
-docker run -d -p 4200:4200 --name birdapp --restart always schuler6/schuler6-3120:latest
+LOCKFILE="/tmp/deploy.lock"
+
+(
+  flock -n 9 || exit 1
+
+  # kill the old container process - stopped & then removed
+  docker stop birdapp || true
+  docker rm birdapp || true
+
+  # pull fresh image
+  docker pull schuler6/schuler6-3120:latest
+
+  # run new container by name, with restart automatic
+  docker run -d -p 4200:4200 --name birdapp --restart always schuler6/schuler6-3120:latest
+
+
+) 9>$LOCKFILE
